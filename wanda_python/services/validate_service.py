f1c610f9c007628b0bd9c2989aef3fb0093e61c5
Lucas Ibiapino <lucas.ibiapino07@gmail.com>
2025-04-13 21:19:08 -0300

    refatorando: mudancas na validaca utilizando as classes auxiliares

--- wanda_python/services/validate_service.py
+++ wanda_python/services/validate_service.py
@@ -1,5 +1,10 @@
 from wanda_python.schema.validate_dto import ValidateRequest, ValidateResponse
 from dotenv import load_dotenv
+from wanda_python.validators.syntax_validator import SyntaxValidator
+from wanda_python.validators.signature_validator import SignatureValidator
+from wanda_python.validators.malicious_checker import MaliciousChecker
+from wanda_python.validators.execution_validator import ExecutionValidator
+
 import json
 
 import os
@@ -11,12 +16,16 @@ class ValidateService:
     def __init__(self):
         load_dotenv()
         self.openai_api_key = os.getenv("OPENAI_API_KEY")
+        self.syntax_validator = SyntaxValidator()
+        self.signature_validator = SignatureValidator()
+        self.malicious_checker = MaliciousChecker()
+        self.execution_validator = ExecutionValidator()
 
     async def validate(self, data: ValidateRequest) -> ValidateResponse:
         code = data.code # Pega a função
 
         # 1 Validação: Sintexe e indentação
-        response_validate = self.validate_sintaxe_and_indentation(code, data.assistantStyle)
+        response_validate = self.syntax_validator.validate(code, data.assistantStyle, self.openai_api_key)
         if response_validate:
             resposta_dict = json.loads(response_validate)
             thought = resposta_dict["pensamento"]
@@ -26,17 +35,17 @@ class ValidateService:
         tree = ast.parse(code) # Vai ser usada nas próximas duas validações (árvore do código)
 
         # 2 Validação: Assinatura e argumentos
-        response_validate = self.validate_signature_and_parameters(tree, data.assistantStyle)
+        response_validate = self.signature_validator.validate_signature_and_parameters(tree, data.assistantStyle)
         if response_validate:
             return ValidateResponse.create(valid=False, answer=response_validate, thought="")
 
         # 3 Validação: Verificando comandos maliciosos
-        malicious_errors = check_for_malicious_code_in_tree(tree)
+        malicious_errors = self.malicious_checker.validate(tree)
         if malicious_errors:
             return ValidateResponse.create(valid=False, answer=malicious_errors, thought="")
         
         # 4 Validação: Executa uma bateria de testes
-        execution_errors = self.validate_execution(code, data.assistantStyle)
+        execution_errors = self.execution_validator.validator(code, data.assistantStyle)
         if execution_errors:
             resposta_dict_execution = json.loads(execution_errors)
             thought = resposta_dict_execution["pensamento"]
